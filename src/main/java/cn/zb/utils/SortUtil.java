daff8bf9cf437653c0d315caa32214d5e46e5cbc
zb <zhangbo@51box.cn>
2018-05-15 22:45:21 +0800

    加入sortUtil类 待完善 用于算法

---
+++ src/main/java/cn/zb/utils/SortUtil.java
@@ -0,0 +1,11 @@
+package cn.zb.utils;
+
+/**
+ * @author zb Created in 7:08 PM 2018/5/15
+ */
+public class SortUtil {
+
+    public static boolean less(Comparable v, Comparable m) {
+        return v.compareTo(m) > 0;
+    }
+}
