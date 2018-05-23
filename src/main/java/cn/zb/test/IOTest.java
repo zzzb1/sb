1f7a873c80fce99eac6f34fb39511770c2497dea
zb <zhangbo@51box.cn>
2018-05-23 23:14:31 +0800

    加入最简单的io测试类-bw br

--- src/main/java/cn/zb/test/IOTest.java
+++ src/main/java/cn/zb/test/IOTest.java
@@ -4,10 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /**
@@ -18,7 +21,42 @@ public class IOTest {
 
     public static void main(String[] args) {
 //        readByte();
-        copyFile();
+//        copyFile();
+        br();
+    }
+
+    /**
+     * 结果换行没了
+     */
+    private static void br() {
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        try {
+            br = new BufferedReader(new FileReader("/Users/zhangbo/Downloads/a.txt"));
+            bw = new BufferedWriter(new FileWriter("/Users/zhangbo/Downloads/c.txt"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                bw.write(line);
+                bw.flush();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (bw != null) {
+                try {
+                    bw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     private static void copyFile() {
